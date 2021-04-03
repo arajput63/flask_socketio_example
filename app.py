ee9e35de6badcda050093862711efefa5fa83ed8
amar <amarrajput63@gmail.com>
2021-04-03 17:47:55 +0100

    added ping and connection response for websockets from edge devices

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ from flask_socketio import SocketIO
 #import random
 #import datetime
 
+from subprocess import check_output
+def get_ip_addr():
+    return check_output(['hostname', '-I']).decode().rstrip()
+
 
 app = Flask(__name__)
 socketio = SocketIO(app=app, cors_allowed_origins='*')
@@ -29,6 +33,10 @@ def socket_page():
 def connect_sock():
     print("[ INFO ] SocketIO Client Connected: {}".format(request.sid))
 
+@socketio.on('connection_message', namespace='/client')
+def connect_sock(msg):
+    print(msg['message'])
+
 @socketio.on('disconnect', namespace='/client')
 def disconnect_sock():
     print("[ INFO ] SocketIO Client Disconnected: {}".format(request.sid))
@@ -45,6 +53,19 @@ def disconnect_web():
 @socketio.on('connect', namespace='/picam_stream')
 def connect_cam():
     print('Picam Connected!')
+
+
+@socketio.on('PING_FROM_CLIENT', namespace='/client')
+def ping_picam(message):
+    print('PING request received: ' + message['picam_ip'])
+    socketio.emit(
+        'PING_FROM_SERVER',
+        {
+            'server_ip': get_ip_addr()
+        },
+        namespace='/server'
+    )
+    print('PING response sent back to client...')
 
 
 if __name__ == "__main__":
--- socketio_client.py
+++ socketio_client.py
@@ -1,18 +1,23 @@
 import socketio
 import datetime
+import time
 
-server_address = "192.168.0.10"
+from subprocess import check_output
+
+#server_address = "192.168.0.10"
+#server_address = "192.168.1.94"
+server_address = "178.79.155.175"
 server_port = 5000
 
 sio = socketio.Client()
 
 @sio.event
 def connect():
-    print("[ INFO ] Successfully connected to the server.")
+    print("[ INFO ] Incoming connection from something...?")
 
 @sio.event
 def connect_error():
-    print("[ INFO ] Failed to connect to the server.")
+    print("[ INFO ] Failed to connect.")
 
 @sio.event
 def disconnect():
@@ -22,9 +27,37 @@ def disconnect():
 print('[ INFO ] Connecting to server http://{}:{}...'.format(
     server_address, server_port))
 
+
+def get_ip_addr():
+    return check_output(['hostname', '-I']).decode().rstrip()
+
+server_connected = False
+
+def ping_server():
+    global server_connected
+    print('Sending ping request to server...')
+    sio.emit(
+        'PING_FROM_CLIENT',
+        {
+            'picam_ip': get_ip_addr()
+        },
+        namespace='/client')
+    server_connected = False
+
+@sio.on('PING_FROM_SERVER', namespace='/server')
+def ping_response(message):
+    global server_connected
+    print('Ping response received: ' + message['server_ip'])
+    server_connected = True
+
+
 sio.connect(
     'http://{}:{}'.format(server_address, server_port),
     transports=['websocket'],
     namespaces=['/client']
 )
 
+while True:
+    ping_server()
+    time.sleep(5)
+    
