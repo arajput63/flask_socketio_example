3d7633f3df7d253b3d838f9cc29e8a83e4d1a7a4
danieljfarrell <daniel.james.farrell@gmail.com>
2021-04-03 11:46:53 +0100

    Adding GitHub Actions script to build and push image

---
+++ scripts/start.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/bash sh
+
+# Exit in case of error
+set -e
+
+echo "No start script"
+
+#echo "RELOAD is $RELOAD"
+#echo "HOST is $HOST"
+#echo "PORT is $PORT"
+
+#if [ -n "$RELOAD" ] && [ $RELOAD = 'true' ]; then
+#    uvicorn app.main:app --host $HOST --port $PORT --reload
+#else
+#    gunicorn "app.main:app" --config "/proj/gunicorn_conf.py" --worker-class "uvicorn.workers.UvicornWorker"
+#fi
+
